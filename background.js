51176f2b1860e228b88d9cb29d6089bc710b41c9
Samuel Labastille <samuellabastille17@gmail.com>
2023-02-21 14:08:52 -0500

    added event listener for the button

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 
 chrome.tabs.onUpdated.addListener((tabId,changeInfo, tab)=>{
-
+  console.log("this is change info" , changeInfo)
    if(tab.url && tab.url.includes("app.slack.com/client")){
         const getChannelName = (tabUrl)=>{
           let preUrl = tabUrl.split('client/')[1]
@@ -8,9 +8,16 @@ chrome.tabs.onUpdated.addListener((tabId,changeInfo, tab)=>{
           return channelName
         }
         const channelName = getChannelName(tab.url)
-      console.log("the channel name is: ", channelName)
-      console.log("I SEE THE TABB DUDES AND DUDETTEssssS", tab.url)
+      // console.log("the channel name is: ", channelName)
+      // console.log("I SEE THE TABB DUDES AND DUDETTEssssS", tab.url)
+      // chrome.tabs.sendMessage(tabId, {
+      //   type: "NEW",
+      //   channelIdL: channelName
+      // })
     
     }
+    // chrome.storage.local.get(["token"]).then((result) => {
+    //   console.log("Value currently is this from background " + result.key);
+    // });
   })
   
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//console.log("we IN DERE")
+
+document.getElementById("button").addEventListener('click',()=>{
+    let token = document.getElementById("token").value;
+    chrome.storage.local.set({ token: token }).then(() => {
+        console.log("Value is set to " + token);
+      });
+    //alert(token)
+}
+
+)
+
